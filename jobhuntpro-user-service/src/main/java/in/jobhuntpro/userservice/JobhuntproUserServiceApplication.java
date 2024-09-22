d2e330d661be1d7f2df7e9ff8b434fd754ee3719
Akshat Jain <akshat.j@loginextsolutions.com>
2024-09-22 21:23:09 +0530

    Intializing User Service Repo

---
+++ jobhuntpro-user-service/src/main/java/in/jobhuntpro/userservice/JobhuntproUserServiceApplication.java
@@ -0,0 +1,13 @@
+package in.jobhuntpro.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobhuntproUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobhuntproUserServiceApplication.class, args);
+	}
+
+}
---
+++ jobhuntpro-user-service/src/test/java/in/jobhuntpro/userservice/JobhuntproUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.jobhuntpro.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobhuntproUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
